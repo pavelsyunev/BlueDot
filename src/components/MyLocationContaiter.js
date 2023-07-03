705dddc553d62df67b86697d88f5223e6a849daf
Pavel Syunev <pavelsyunev@Pavels-iMac.home>
2023-07-03 14:43:57 -0400

    Updated button color

--- src/components/MyLocationContaiter.js
+++ src/components/MyLocationContaiter.js
@@ -3,11 +3,11 @@ import { TbLocationFilled } from "react-icons/tb";
 const MyLocationContainer = ({userCurrentLocation, isUserCurrentLocation}) => {
     return (
         <button
-            className="rounded-full shadow-md bg-white absolute z-50 bottom-0 right-0 m-6 p-5 border-2 border-blue-500"
+            className="rounded-full shadow-md absolute z-50 bottom-0 right-0 m-6 p-5 bg-blue-700"
             onClick={userCurrentLocation}
             disabled={!isUserCurrentLocation}
         >
-            <TbLocationFilled color="#3b82f6" />
+            <TbLocationFilled color="white" />
         </button>
     )
 }
